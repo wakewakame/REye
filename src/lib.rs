87446a508f3695b54e538ecfd860f8d485ec22d4
wakewakame <0214.taiki@gmail.com>
2021-02-03 02:32:00 +0900

    一旦コミット

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,36 @@
+// デバッグビルド時にpanicの内容をconsole.errorに出力するようにする
 #[cfg(debug_assertions)]
 extern crate console_error_panic_hook;
 #[cfg(debug_assertions)]
 use std::panic;
+
 use wasm_bindgen::prelude::*;
 
+// wasmの初期化時に呼ばれる関数
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
+    // デバッグビルド時にpanicの内容をconsole.errorに出力するようにする
     #[cfg(debug_assertions)]
     panic::set_hook(Box::new(console_error_panic_hook::hook));
-
-    let window = web_sys::window().expect("no global `window` exists");
-    let document = window.document().expect("should have a document on window");
-    let body = document.body().expect("document should have a body");
-
-    let val = document.create_element("p")?;
-    val.set_inner_html("Hello from Rust!");
-
-    body.append_child(&val)?;
 
     Ok(())
 }
 
 #[wasm_bindgen]
-pub fn add(a: u32, b: u32) -> u32 {
-    a + b
+#[derive(Debug)]
+pub struct RootComponent {
+    i: i32,
+}
+
+#[wasm_bindgen]
+impl RootComponent {
+    pub fn new() -> RootComponent {
+        RootComponent { i: 0 }
+    }
+    pub fn add(&mut self) {
+        self.i += 1;
+    }
+    pub fn get(&self) -> i32 {
+        self.i
+    }
 }
