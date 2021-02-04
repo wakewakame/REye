08e8618befcdf21f290dcdb72dbdf7872b32bf3a
wakewakame <0214.taiki@gmail.com>
2021-02-04 17:22:26 +0900

    マウスイベントの処理を追加

--- src/lib.rs
+++ src/lib.rs
@@ -19,32 +19,66 @@ pub fn main() -> Result<(), JsValue> {
     Ok(())
 }
 
+#[derive(Debug)]
+struct Mouse {
+    x: f64, y: f64,
+}
+
+impl Mouse {
+    fn new() -> Mouse {
+        Mouse { x: 0.0, y: 0.0 }
+    }
+    fn apply_event(&mut self, event: web_sys::MouseEvent) {
+        self.x = event.offset_x() as f64;
+        self.y = event.offset_y() as f64;
+    }
+}
+
 #[wasm_bindgen]
 #[derive(Debug)]
 pub struct RootComponent {
     width: f64, height: f64,
+    mouse: Mouse,
 }
 
 #[wasm_bindgen]
 impl RootComponent {
     pub fn new() -> RootComponent {
-        RootComponent { width: 0.0, height: 0.0 }
+        RootComponent {
+            width: 0.0, height: 0.0,
+            mouse: Mouse::new()
+        }
     }
     pub fn draw(&mut self, context2d: js_sys::Object) -> Result<(), JsValue> {
         // 引数の型チェック
-        if let Ok(context2d) = context2d.dyn_into::<web_sys::CanvasRenderingContext2d>() {
-            // 描画
-            context2d.set_fill_style(&JsValue::from_str("#f0f"));
-            context2d.fill_rect(10.0, 10.0, 100.0, 100.0);
-        }
-        else {
+        if !context2d.has_type::<web_sys::CanvasRenderingContext2d>() {
 			return Err(JsValue::from_str("the argument value is not instance of CanvasRenderingContext2d"));
         }
+        let context2d = context2d.dyn_into::<web_sys::CanvasRenderingContext2d>()?;
+
+        // 描画
+        context2d.set_fill_style(&JsValue::from_str("#fff"));
+        context2d.fill_rect(0.0, 0.0, self.width, self.height);
+        context2d.set_fill_style(&JsValue::from_str("#f0f"));
+        context2d.fill_rect(self.mouse.x, self.mouse.y, 100.0, 100.0);
 
         Ok(())
     }
     pub fn resize(&mut self, width: f64, height: f64) {
         self.width = width;
         self.height = height;
+    }
+    pub fn dispatch_event(&mut self, event: js_sys::Object) -> Result<(), JsValue> {
+        // 引数の型チェック
+        if !event.has_type::<web_sys::Event>() {
+			return Err(JsValue::from_str("the argument value is not instance of Event"));
+        }
+
+        if let Ok(e) = event.dyn_into::<web_sys::MouseEvent>() { self.mouse(e); }
+
+        Ok(())
+    }
+    fn mouse(&mut self, e: web_sys::MouseEvent) {
+        self.mouse.apply_event(e);
     }
 }
