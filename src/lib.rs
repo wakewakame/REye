91ae0b9db9162f32baa4574a305c0183b5913f74
wakewakame <0214.taiki@gmail.com>
2021-02-04 14:38:36 +0900

    rustからcanvasに描画

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,9 @@
-// デバッグビルド時にpanicの内容をconsole.errorに出力するようにする
-#[cfg(debug_assertions)]
 extern crate console_error_panic_hook;
-#[cfg(debug_assertions)]
-use std::panic;
-
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
 
 // println!の代わりにlog!でコンソール出力できるようにする
+#[allow(unused_macros)]
 macro_rules! log {
     ( $( $t:tt )* ) => {
         web_sys::console::log_1(&format!( $( $t )* ).into());
@@ -16,9 +13,8 @@ macro_rules! log {
 // wasmの初期化時に呼ばれる関数
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
-    // デバッグビルド時にpanicの内容をconsole.errorに出力するようにする
-    #[cfg(debug_assertions)]
-    panic::set_hook(Box::new(console_error_panic_hook::hook));
+    // panicの内容をconsole.errorに出力するようにする
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
 
     Ok(())
 }
@@ -26,22 +22,29 @@ pub fn main() -> Result<(), JsValue> {
 #[wasm_bindgen]
 #[derive(Debug)]
 pub struct RootComponent {
-    i: i32,
+    width: f64, height: f64,
 }
 
 #[wasm_bindgen]
 impl RootComponent {
     pub fn new() -> RootComponent {
-        RootComponent { i: 0 }
+        RootComponent { width: 0.0, height: 0.0 }
     }
-    pub fn draw(&mut self, context: web_sys::CanvasRenderingContext2d) -> web_sys::CanvasRenderingContext2d {
-		log!("{:?}", context);
-        return context;
+    pub fn draw(&mut self, context2d: js_sys::Object) -> Result<(), JsValue> {
+        // 引数の型チェック
+        if let Ok(context2d) = context2d.dyn_into::<web_sys::CanvasRenderingContext2d>() {
+            // 描画
+            context2d.set_fill_style(&JsValue::from_str("#f0f"));
+            context2d.fill_rect(10.0, 10.0, 100.0, 100.0);
+        }
+        else {
+			return Err(JsValue::from_str("the argument value is not instance of CanvasRenderingContext2d"));
+        }
+
+        Ok(())
     }
-    pub fn add(&mut self) {
-        self.i += 1;
-    }
-    pub fn get(&self) -> i32 {
-        self.i
+    pub fn resize(&mut self, width: f64, height: f64) {
+        self.width = width;
+        self.height = height;
     }
 }
